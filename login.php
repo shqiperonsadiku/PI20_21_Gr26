5173d5a2f1180420ab85cd3c51092eeabee616c7
shqiperonsadiku <74976264+shqiperonsadiku@users.noreply.github.com>
2021-05-19 18:33:30 +0200

    sign up and login forms with php

--- login.php
+++ login.php
@@ -1,3 +1,42 @@
+<?php
+
+session_start();
+$errors = array('email'=>'', 'username'=>'', 'password'=>'');
+$host = 'localhost';
+$user = 'id15893390_userdb';
+$pass = 'Shqiperon123.';
+$db = 'id15893390_userregistration';
+$con =mysqli_connect($host, $user, $pass, $db);
+
+$email = $_POST['email'];
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+mysqli_select_db($con, $db);
+
+if(isset($_POST['submit'])) {    
+        
+        //$s = " select * from usertable where username='$username' && password = '$password'";
+        $s = " select * from usertable where username='$username'";
+        $result = mysqli_query($con, $s);
+
+        $num = mysqli_num_rows($result);
+
+        if($num == 1){    
+            while($row=mysqli_fetch_assoc($result)){
+                if(password_verify($password, $row['password'])){
+                    $_SESSION['username'] = $username;
+                    header('location:homepage.php');
+                }else {
+                    $errors['username'] = 'Username or password incorrect!';
+                }   
+            }
+        }else {
+            $errors['username'] = 'Username or password incorrect!';
+        }
+    }
+
+?>
 <html>
 
 <head>
@@ -13,22 +52,19 @@
             <div class="row">
                 <div class="col-md-6">
                     <h2>Login Here</h2>
-                    <form action="validation.php" method="post">
+                    <form action="login.php" method="post">
                         <div class="form-group">
                             <label>Username or Email</label>
-                            <input type="text" name="username" class="form-control" required>
+                            <input type="text" name="username" class="form-control">
                         </div>
                         <div class="form-group">
                             <label>Password</label>
-                            <input type="password" name="password" class="form-control" required>
+                            <input type="password" name="password" class="form-control">
+                            <div class="red-text"><?php echo $errors['username']; ?></div>
                         </div>
-                        <p class="signup-now">You don't have an account yet? <a href="signup.html" class="signup-nowlink">Sign Up</a> now!</p>
-                        <button type="submit" class="btn btn-primary">Log in</button>
-                        <p>Check the box if you agree with our
-                            <a href="termsConditons.html" target="_blank">Terms and Conditions</a> and
-                            <a href="privacyPolicy.html" target="_blank">Privacy & Policy</a>
-                            <input class="tickInput" type="checkbox" name="termsAndConditions" value="Yes"> Yes
-                        </p>
+                        <p class="signup-now">You don't have an account yet? <a href="signup.php" class="signup-nowlink">Sign Up</a> now!</p>
+                         <p class="signup-now">Or continue as a <a href="index.html" class="signup-nowlink">guest</a> </p>
+                        <button type="submit" name='submit' value='submit' class="btn btn-primary">Log in</button>
                     </form>
                 </div>
 
--- signup.php
+++ signup.php
@@ -1,5 +1,73 @@
-<html>
+<?php
+session_start();
+$email = $username = $password = '';
+$errors = array('email'=>'', 'username'=>'', 'password'=>'');
 
+$host = 'localhost';
+$user = 'id15893390_userdb';
+$pass = 'Shqiperon123.';
+$db = 'id15893390_userregistration';
+$con =mysqli_connect($host, $user, $pass, $db);
+
+mysqli_select_db($con, $db);
+
+if(isset($_POST['submit'])) {    
+    if(empty( $_POST['email'])) {
+        $errors['email'] ='Email is required!<br/>';
+    } else {
+        $email = $_POST['email'];
+        
+        if(!filter_var( $_POST['email'], FILTER_VALIDATE_EMAIL)){
+            $errors['email'] = 'Enter a valid email address!';
+        }else{
+            $s = " select * from usertable where email='$email'";
+            $result = mysqli_query($con, $s);
+            $num = mysqli_num_rows($result);
+            if($num == 1){    
+                $errors['email'] = 'You already have an account with this email!';
+            }
+        }
+    }
+
+    if(empty($_POST['username'])) {
+        $errors['username'] = 'Username is required! <br/>';
+    } else {
+        $username = $_POST['username'];
+        if(!preg_match('/^[a-zA-z\s]+$/', $username)){
+            $errors['username'] = 'Username must be letters and spaces only!';
+        }else {
+            $s = " select * from usertable where email='$email'";
+            $result = mysqli_query($con, $s);
+            $num = mysqli_num_rows($result);
+            if($num == 1){    
+                $errors['username'] = 'Username already taken!';
+            }
+        }
+    }
+
+    if(empty($_POST['password'])) {
+        $errors['password'] = 'Password is required! <br/>';
+    } else if(isset($_POST['password'])){
+        $password = $_POST['password'];
+        $number = preg_match('@[0-9]@', $password);
+        $uppercase = preg_match('@[A-Z]@', $password);
+        $lowercase = preg_match('@[a-z]@', $password);
+        $specialChars = preg_match('@[^\w]@', $password);
+        if(strlen($password) < 8 || !$number || !$uppercase || !$lowercase || !$specialChars){
+            $errors['password'] =  'Password must be at least 8 characters in length and must contain at least one number, one upper case letter, one lower case letter and one special character.';
+        } else {
+                $passHashed = password_hash($password, PASSWORD_DEFAULT);
+                $reg = " insert into usertable(email, username, password) values ('$email', '$username','$passHashed')";
+                mysqli_query($con, $reg);
+                //$_SESSION['username'] = $username;
+                header('location:login.php');
+            }
+        }
+        
+    }
+?>
+
+<html>
 <head>
     <title>Sign up | Health & Fitness</title>
     <link rel="icon" type="image/jpg" href="images/logo1.png" />
@@ -7,35 +75,29 @@
 </head>
 
 <body>
-<?php include 'registration.php';?>
     <div class="container">
         <div class="login-box">
             <div class="row">
                 <div class="col-md-6">
                     <h2>Sign Up to our newsletter</h2>
-                    <form action="registration.php" method="post">
+                    <form action="signup.php" method="post">
                         <div class="form-group">
                             <label>Email</label>
-                            <input type="email" name="email" class="form-control" required>
+                            <input type="email" name="email" class="form-control" value="<?php echo $email ?>">
                             <div class="red-text"><?php echo $errors['email']; ?></div>
                         </div>
                         <div class="form-group">
                             <label>Username</label>
-                            <input type="text" name="username" class="form-control" required>
-                            <div class="red-text"><?php echo $errors['username'];?></div>
+                            <input type="text" name="username" class="form-control" value="<?php echo $username ?>">
+                            <div class="red-text"><?php echo $errors['username']; ?></div>
                         </div>
                         <div class="form-group">
                             <label>Password</label>
-                            <input type="password" name="password" class="form-control" required>
-                            <div class="red-text"><?php echo $errors['password'];?></div>
+                            <input type="password" name="password" class="form-control" value="<?php echo $password?>">
+                            <div class="red-text"><?php echo $errors['password']; ?></div>
                         </div>
-                        <p class="signup-now">You already have an account.<a href="login.html" class="signup-nowlink">Log in</a> now!</p>
-                        <button type="submit" class="btn btn-primary">Sign Up</button>
-                        <p>Check the box if you agree with our
-                            <a href="termsConditons.html" target="_blank">Terms and Conditions</a> and
-                            <a href="privacyPolicy.html" target="_blank">Privacy & Policy</a>
-                            <input class="tickInput" type="checkbox" name="termsAndConditions" value="Yes"> Yes
-                        </p>
+                        <p class="signup-now">You already have an account.<a href="login.php" class="signup-nowlink">Log in</a> now!</p>
+                        <button type="submit" name='submit' value='submit' class="btn btn-primary">Sign Up</button>
                     </form>
                 </div>
 
